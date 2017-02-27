ddb22a4dbe81c90af196588dee9e71e8952fc3b8
Steven Hadley <sphadley@gmail.com>
2017-02-26 23:40:58 -0600

    Update Assignment 9 to Leaflet and add circleMarkers

--- ElasticBeanstalk/static/js/assignment9.js
+++ ElasticBeanstalk/static/js/assignment9.js
@@ -1,42 +1,69 @@
 var Markers = new Array();
 var Map;
+var locations;
 
-function fitToBounds() {
-    var bounds = new google.maps.LatLngBounds();
-    for (var m in Markers) 
+function getColor(year){
+    var numYear = parseInt(year);
+    var currentYear = new Date().getFullYear();
+    var yearsOld = currentYear - numYear;
+    if(yearsOld < 5)
     {
-        bounds.extend(Markers[m].getPosition());
+        return '#0868ac';
     }
-    Map.fitBounds(bounds);
+    else if (yearsOld <10)
+    {
+        return '#43a2ca';
+    }
+    else if (yearsOld < 15)
+    {
+        return '#7bccc4';
+    }
+    else if(yearsOld < 20)
+    {
+        return '#bae4bc';
+    }
+    else
+    {
+        return '#f0f9e8';
+    }
 }
 
-function getBreweries()
-{
+function getBreweries() {
+    locations = {};
     for(var m in Markers)
     {
-        Markers[m].setMap(null);
+        Map.removeLayer(Markers[m]);
     }
     Markers = [];
     $("#output").empty();
-    var en = '{"iv":"DKfFCHFekKcCMjTpay+tIg==","v":1,"iter":1000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"kK+ungiKfDA=","ct":"Xo8m2QLfqhjeM6qQG4gAT/khTtEZDRm2Juxbp6qPtIGO4U+aQ9l/0Q=="}';
-    var pass = $("#password").val();
     $.get( "bdb/v2/locations", 
         { region: $("#state").val(), locationType: "micro, macro" }).done(function(data) {
             for(var loc in data.data)
             {
-                $.get("bdb/v2/brewery/" + data.data[loc].breweryId,
-                { withLocations:'Y'}).done(function(brewer) {
-                    if(brewer.data.images != null)
-                    {
-                        $("#output").append('<div class="logoBox" > <img class="brewerLogo" src="' + brewer.data.images.large + '"/>'+ brewer.data.name+ '</div>');
-                        Markers.push(new google.maps.Marker({
-                            position: {lat: brewer.data.locations[0].latitude, 
-                                        lng: brewer.data.locations[0].longitude},
-                            map:Map,
-                            title:brewer.data.name 
-                        }));
-                        fitToBounds();
-                    }
+                locations[data.data[loc].breweryId] = { 
+                    lat: data.data[loc].latitude, 
+                    lon: data.data[loc].longitude,
+                    name: data.data[loc].brewery.name,
+                    type: data.data[loc].locationType,
+                    yearOpened: data.data[loc].yearOpened,
+                    openToPublic: data.data[loc].openToPublic  };
+
+                $.get("bdb/v2/brewery/" + data.data[loc].breweryId +"/beers",
+                { withBreweries:'Y'}).done(function(beers) {
+                        var bid = beers.data[0].breweries[0].id;
+                        locations[bid].beers = beers.data;
+                        //$("#output").append('<div class="logoBox" > <img class="brewerLogo" src="' + brewer.data.images.large + '"/>'+ brewer.data.name+ '<br>' + brewer.data.locations[0].locationType + '<br>' + brewer.data.locations[0].openToPublic + '</div>');
+                        $("#output").append(beers.data.length + "<br>");
+                        var ltlng = L.latLng(locations[bid].lat, locations[bid].lon);
+                        var m = L.circleMarker(ltlng,  {
+                            radius: locations[bid].beers.length, 
+                            color: 'black',
+                            fillColor:getColor(locations[bid].yearOpened),
+                            fillOpacity: 0.9});
+                        m.bindPopup(locations[bid].name);
+                        Markers.push(m);
+                        Map.addLayer(m);
+                        
                 });
             }
                                                     //$("#output").html(brewIds);
@@ -44,6 +71,11 @@ function getBreweries()
 
 }
 $("document").ready(() => {
+    Map =  L.map('map').setView([42.0, -85.634], 2);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(Map); 
+    
     $("#begin").click(function() {
         getBreweries()});
     $("input:radio[name=font]").click(function() {
@@ -64,12 +96,4 @@ $("document").ready(() => {
     });
 });
 
-function initMap() {
-    var uluru = {lat: 43.444055, lng: -93.220486};
-    Map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: uluru
-    });
-    
-}
 
