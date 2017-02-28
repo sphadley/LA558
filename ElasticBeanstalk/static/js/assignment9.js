7ac22ad47d3adde4c8564ca99059c9b793f67b0c
Steven Hadley <sphadley@gmail.com>
2017-02-27 20:34:39 -0600

    Finish Assignment9

--- ElasticBeanstalk/static/js/assignment9.js
+++ ElasticBeanstalk/static/js/assignment9.js
@@ -37,7 +37,7 @@ function getBreweries() {
     Markers = [];
     $("#output").empty();
     $.get( "bdb/v2/locations", 
-        { region: $("#state").val(), locationType: "micro, macro" }).done(function(data) {
+        { region: "Iowa", locationType: "micro, macro" }).done(function(data) {
             for(var loc in data.data)
             {
                 locations[data.data[loc].breweryId] = { 
@@ -53,7 +53,6 @@ function getBreweries() {
                         var bid = beers.data[0].breweries[0].id;
                         locations[bid].beers = beers.data;
                         //$("#output").append('<div class="logoBox" > <img class="brewerLogo" src="' + brewer.data.images.large + '"/>'+ brewer.data.name+ '<br>' + brewer.data.locations[0].locationType + '<br>' + brewer.data.locations[0].openToPublic + '</div>');
-                        $("#output").append(beers.data.length + "<br>");
                         var ltlng = L.latLng(locations[bid].lat, locations[bid].lon);
                         var m = L.circleMarker(ltlng,  {
                             radius: locations[bid].beers.length, 
@@ -71,29 +70,27 @@ function getBreweries() {
 
 }
 $("document").ready(() => {
-    Map =  L.map('map').setView([42.0, -85.634], 2);
+    Map =  L.map('map').setView([41.6005, -93.6091], 7);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(Map); 
-    
-    $("#begin").click(function() {
-        getBreweries()});
-    $("input:radio[name=font]").click(function() {
-        var value = $(this).val();
-        $(".logoBox").css({fontFamily: value});
-    });
-
-    $("input:radio[name=fontsize]").click(function() {
-        var value = $(this).val();
-        $(".logoBox").css({fontSize: value});
-    });
-
-    $(function() {
-        $( "#slider" ).slider({min:50, max:500}).on( "slidechange", function( event, ui ) 
-        {
-            $(".brewerLogo").css({width: ui.value+"px"});
-        });
-    });
+    getBreweries();
+    var icon1 = L.icon({iconUrl: "static/istate.png", iconSize: [50,40]});
+    L.marker([42.0269133, -93.6519128], {icon:icon1}).bindPopup("<h3>Iowa State University</h3> <br>Iowa State University of Science and Technology is a public flagship[4] land-grant and space-grant"
+        + "research university located in Ames, Iowa, United States. Iowa State is classified as a Research University with very high research activity (RU/VH) by the Carnegie Foundation for"
+        + "the Advancement of Teaching.[5] Iowa State is also a member of the Association of American Universities (AAU), which consists of 62 leading research universities in North America."
+        + "[6] Iowa State has also been designated an \"Innovation and Economic Prosperity University,\" a designation awarded to only 54 public universities in the U.S.[7]"
+        + "Founded in 1858 and coeducational from its start, Iowa State became the nation’s first designated land-grant institution when the Iowa Legislature accepted the"
+         + "provisions of the 1862 Morrill Act on September 11, 1862, making Iowa the first state in the nation to do so.[8]"
+        + " Iowa States academic offerings are administered today through eight colleges, including the graduate college, that offer over 100 bachelor's degree programs,"
+        + " 112 master's degree programs, and 83 at the Ph.D. level, plus a professional degree program in Veterinary Medicine.[9]  --Wikipedia authors: https://en.wikipedia.org/wiki/Iowa_State_University", { maxHeight: 200, offset:[1,1] }).addTo(Map);    
+    L.marker([41.6710, -93.7130]).bindPopup(
+        "Johnston, IA <br> <img src='static/johnston2.jpg'> </img>", {
+            maxWidth: 400,
+            closeButton: true 
+        }).addTo(Map);
+    var divCon = L.divIcon({html: "Iowa <br> <img src='static/iowa2.png'> </img>", iconSize: [50,50]});
+    L.marker([41.8780, -93.0977], {icon: divCon}).bindPopup("Iowa").addTo(Map);
 });
 
 
