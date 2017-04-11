ea445b8cd9cb423223ad66c1707483e2b2ae77b8
Steven Hadley <sphadley@gmail.com>
2017-04-10 23:15:03 -0500

    pushing to github

---
+++ Assignment19/index.js
@@ -0,0 +1,40 @@
+var map;
+
+function showPosition(position) {
+    mapsetView([postion.coords.latitude, postion.coords.longitude], 12);
+    console.log(position.coords.latitude);
+    console.log(position.coords.longitude);
+};
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else 
+    {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+$('document').ready(() => {
+    var osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    });
+
+    map =  L.map('map', {
+       center: [41.577, -93.231], 
+        zoom: 3,
+        layers: [osm /*, flights*/ ]
+    });
+
+
+    var baseMaps = {
+      "OSM": osm,
+    };
+
+    var overlayMaps = {
+    };
+
+
+    L.control.layers(baseMaps, overlayMaps).addTo(map);
+    
+    getLocation();
+});
